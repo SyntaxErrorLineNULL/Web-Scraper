c3c5dd678e988b5f0cd18edd7d16f57a3eba0569
syntaxErrorLineNil <cyberorange16@gmail.com>
2024-10-03 22:29:47 +0300

    redis conf

--- config/config.go
+++ config/config.go
@@ -17,6 +17,15 @@ type (
 		ConnectTimeout int    `toml:"connect_timeout"`
 		PoolSize       int    `toml:"pool_size"`
 	}
+
+	// Redis holds configuration parameters for connecting to multiple Redis instances.
+	Redis struct {
+		Redis          []string `toml:"redis"`
+		ConnectionSize int      `toml:"pool_size"`
+		DialTimeout    int64    `toml:"dial_timeout"`
+		WriteTimeout   int64    `toml:"write_timeout"`
+		ReadTimeout    int64    `toml:"read_timeout"`
+	}
 )
 
 func NewConfig(filePath string) *Config {
