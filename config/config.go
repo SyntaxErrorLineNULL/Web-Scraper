0f432f0b1de739b1a7c6c57f3777a6996bcb6bb4
syntaxErrorLineNil <cyberorange16@gmail.com>
2024-10-03 22:22:45 +0300

    database config

--- config/config.go
+++ config/config.go
@@ -4,6 +4,19 @@ type (
 	Config struct {
 		filePath string
 	}
+
+	App struct {
+		// Listen specifies the address and port on which the application should listen for incoming requests.
+		Listen string `toml:"listen"`
+		// WorkerCount specifies the number of worker routines that the application should spawn.
+		WorkerCount int `toml:"workerCount"`
+	}
+
+	DataBase struct {
+		URL            string `toml:"url"`
+		ConnectTimeout int    `toml:"connect_timeout"`
+		PoolSize       int    `toml:"pool_size"`
+	}
 )
 
 func NewConfig(filePath string) *Config {
